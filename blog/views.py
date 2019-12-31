4e7d9267173856d90060efd84573d367d4e36e77
Maiya <maiyabourland@gmail.com>
2019-12-30 19:06:44 -0700

    Modified templates to display CSS changes.

--- blog/views.py
+++ blog/views.py
@@ -5,4 +5,4 @@ from .models import BlogPost
 # Create your views here. create a function (def) called post_list that takes request and will return the value it gets from calling function render
 def post_list(request):
     posts = BlogPost.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    return render(request, 'blog/post_list.html', {'posts': posts})
+    return render(request, 'blog/post_list.html', {'BPost': posts})
